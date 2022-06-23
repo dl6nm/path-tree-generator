0ad53353d6ba182e2cdf0b064136f8ef38f026c4
dl6nm <mail@dl6nm.de>
2022-06-23 16:11:10 +0200

    test: add test for human readable representation of a path tree

--- tests/test_path_tree_generator.py
+++ tests/test_path_tree_generator.py
@@ -3,13 +3,15 @@ import json
 from path_tree_generator import PathTree
 
 
-def test_path_tree(shared_datadir):
-    pt = PathTree(
-        root_dir=shared_datadir,
-        dir_only=False
-    )
-
+def test_path_tree_dict(shared_datadir):
+    pt = PathTree(root_dir=shared_datadir)
     data_file = (shared_datadir/'data.json')
-    data = json.load(data_file.open())
-
+    data = json.load(data_file.open(encoding='utf-8'))
     assert pt.dict() == data
+
+
+def test_path_tree_human_readable(shared_datadir):
+    pt = PathTree(root_dir=shared_datadir)
+    data_file = (shared_datadir/'data.tree')
+    data = data_file.open(encoding='utf-8').read()
+    assert pt.human_readable() == data
