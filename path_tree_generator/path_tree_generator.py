bafcd0d08a133c98abd9ea49ce63cf55f15a0f8e
dl6nm <mail@dl6nm.de>
2022-06-27 17:16:04 +0200

    feat: implement building of a path tree

--- path_tree_generator/path_tree_generator.py
+++ path_tree_generator/path_tree_generator.py
@@ -7,7 +7,9 @@ from .models.list_entries import ListEntry, ListEntryType
 
 
 class PathTree:
-    def __init__(self, root_dir):
+    def __init__(self, root_dir: str | pathlib.Path):
+        if isinstance(root_dir, str):
+            root_dir = pathlib.Path(root_dir)
         self._generator = _PathTreeGenerator(root_dir=root_dir)
 
     def dict(self):
@@ -25,21 +27,49 @@ class _PathTreeGenerator:
 
     def get_tree(self) -> list[ListEntry]:
         if not self._tree_built:
-            self._build_tree()
+            self._build_tree(self._root_dir)
             self._tree_built = True
         return self._tree_list
 
-    def _build_tree(self):
-        if self._root_dir.is_dir():
-            # self._root_dir.iterdir()
-            children: list[ListEntry] = []
-            for entry in self._root_dir.iterdir():
-                children = self._add_entry(entry)
-            self._tree_list.append(
-                self._get_dir_entry(path=self._root_dir, children=children)
-            )
+    def _build_tree(self, path: pathlib.Path) -> list[ListEntry]:
+        entries: list[ListEntry] = []
+        if path.is_dir():
+            for entry in path.iterdir():
+                if entry.is_dir():
+                    entries.append(
+                        ListEntry(
+                            entry_type=ListEntryType.dir,
+                            name=entry.name,
+                            path=entry,
+                            children=self._build_tree(entry),
+                        )
+                    )
+                if entry.is_file():
+                    entries.append(
+                        ListEntry(
+                            entry_type=ListEntryType.file,
+                            name=entry.name,
+                            path=entry,
+                        )
+                    )
+                    print(f'   {entry.name} is a FILE')
 
-        print(self._tree_list)
+        self._tree_list
+        self._tree_built = True
+        print('entries: ', entries)
+        return entries
+
+    def _add_entry(self, path: pathlib.Path):
+        entries: list[ListEntry] = []
+        if path.is_dir():
+            entries
+            print(f'{path} is a DIR')
+            entries.append(
+                self._get_dir_entry(path)
+            )
+        else:
+            print(f'{path} is a FILE')
+        return entries
 
     def _get_dir_entry(self, path: pathlib.Path, children: list[ListEntry]):
         return ListEntry(
